de57ad7a0b2265a1f0c586fa11fecdad1b023232
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-12-27 19:43:29 -0600

    convert console.log into a const cursoSeleccionado

--- 01-carrito/js/app.js
+++ 01-carrito/js/app.js
@@ -22,6 +22,8 @@ function agregarCurso(e) {
     // Click en el elemento con la clase .agregar-carrito <a> y evitando event bubbling...
     if (e.target.classList.contains('agregar-carrito')) {
         // ...conocer el padre del botón: .info-card y su padre a su vez: .card, para leer el contenido y mostrarlo en el carrito (nombre, precio e imagen)
-        console.log(e.target.parentElement.parentElement);
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+
+        leerDatosCurso();
     }
 }
