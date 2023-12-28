3c30ef2d8f35b1432139f7f465096d8acb2504bf
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-12-27 19:34:16 -0600

    agregarCurso function

--- 01-carrito/js/app.js
+++ 01-carrito/js/app.js
@@ -13,3 +13,15 @@ function cargarEventListener() {
     // Cuando agregas un curso presionando 'agregar al carrito'
     listaCursos.addEventListener('click', agregarCurso);
 }
+
+// Funciones
+function agregarCurso(e) {
+    // Evitando la acción natural del botón (#)
+    e.preventDefault();
+
+    // Si presionamos en el elemento que contiene la clase .agregar-carrito (el botón) y evitando el event bubbling...
+    if (e.target.classList.contains('agregar-carrito')) {
+        // ...conocer el padre del botón: .info-card, para leer el contenido y mostrarlo en el carrito (nombre, precio e imagen)
+        console.log(e.target.parentElement);
+    }
+}
