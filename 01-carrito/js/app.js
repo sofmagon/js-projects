342ff6ca0f9a9f13530129910a112293636250af
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-12-27 19:59:37 -0600

    creating an empty array articulosCarrito

--- 01-carrito/js/app.js
+++ 01-carrito/js/app.js
@@ -5,6 +5,7 @@ const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+let articulosCarrito = [];
 
 // Eventos
 cargarEventListener();
