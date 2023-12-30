c76fda5dc8fd09e209612dc25590293767bb3be2
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-12-29 21:17:32 -0600

    initial commit

---
+++ 02-enviar-email/js/app.js
@@ -0,0 +1,9 @@
+//:: Ejercicio: Simular el envío de un email, validando todos los campos
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Variables
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+});
