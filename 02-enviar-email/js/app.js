054ee292795a569999890f3c2c615308baa7a6b6
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-12-29 21:21:42 -0600

    adding events to inputs

--- 02-enviar-email/js/app.js
+++ 02-enviar-email/js/app.js
@@ -6,4 +6,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
 
+    // Eventos.- callback: cuando un evento ocurre y se dispara una función
+    inputEmail.addEventListener('blur', function (e) {
+        // Conociendo lo que escribió el usuario
+        console.log(e.target.value);
+    });
+
+    inputAsunto.addEventListener('blur', function (e) {
+        console.log(e.target.value);
+    });
+
+    inputMensaje.addEventListener('blur', function (e) {
+        console.log(e.target.value);
+    });
 });
