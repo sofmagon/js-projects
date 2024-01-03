035abdf2b85d0e8730b5ef4b08a06476625e35b7
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-02 18:57:10 -0600

    feat: mostrarError function

--- 04-localStorage/js/app.js
+++ 04-localStorage/js/app.js
@@ -11,14 +11,31 @@ function eventListeners() {
 }
 
 // Funciones
+
 function agregarTweet(e) {
     e.preventDefault();
     // Área donde el usuario escribe
     const tweet = document.querySelector('#tweet').value;
     // Validación
     if (tweet === '') {
-        console.log('...No puede ir vacío');
+        mostrarError('El campo de texto no puede ir vacío');
         // evita que se ejecuten más líneas, aplica dentro de un if de una función
         return;
     }
+}
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertando en HTML
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Eliminando alerta después de 3s
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
