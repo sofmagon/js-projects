59cb2ca5e0911a17eba25770ef28a0c0ea2dc266
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-02 18:52:42 -0600

    feat: validating if the textarea is empty

--- 04-localStorage/js/app.js
+++ 04-localStorage/js/app.js
@@ -13,4 +13,12 @@ function eventListeners() {
 // Funciones
 function agregarTweet(e) {
     e.preventDefault();
+    // Área donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+    // Validación
+    if (tweet === '') {
+        console.log('...No puede ir vacío');
+        // evita que se ejecuten más líneas, aplica dentro de un if de una función
+        return;
+    }
 }
