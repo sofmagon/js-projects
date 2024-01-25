bb85e0b73298c92238150d8249f3bca6d5fe9f55
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-25 13:29:43 -0600

    add: prototype on UI

--- 06-seguros-prototypes/js/app.js
+++ 06-seguros-prototypes/js/app.js
@@ -7,3 +7,18 @@ function Seguro(marca, year, tipo) {
 }
 
 function UI() { }
+
+// Llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+	const max = new Date().getFullYear();
+	const min = max - 20;
+	const selectYear = document.querySelector('#year');
+
+	// Iterando sobre el año máximo hasta el mínimo creando las diferentes opciones del select
+	for (let i = max; i >= min; i--) {
+		let option = document.createElement('OPTION');
+		option.value = i;
+		option.textContent = i;
+		selectYear.appendChild(option);
+	}
+}
