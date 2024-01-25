dde4ee39aa67f3cc86295cef71bf7e49f69f3174
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-25 13:27:49 -0600

    feat: 2 object constructors

---
+++ 06-seguros-prototypes/js/app.js
@@ -0,0 +1,9 @@
+// Constructores. Uno para la cotización del seguro y otro para la interfaz de usuario
+
+function Seguro(marca, year, tipo) {
+	this.marca = marca;
+	this.year = year;
+	this.tipo = tipo;
+}
+
+function UI() { }
