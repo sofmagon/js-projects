daa41fb33e5b899646e4dde7431c498a73888579
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-28 00:13:29 -0600

    verifying if there's a previous alert

--- 13-pixabay-api/js/app.js
+++ 13-pixabay-api/js/app.js
@@ -19,17 +19,21 @@ function validarFormulario(e) {
 }
 
 function mostrarAlerta(mensaje) {
-	// Creando alerta con scripting
-	const alerta = document.createElement('P');
-	alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
-	alerta.innerHTML = `
+	// Verificar si ya existe una alerta para evitar duplicidad
+	const existeAlerta = document.querySelector('.bg-red-100');
+	if (!existeAlerta) {
+		// Creando alerta con scripting
+		const alerta = document.createElement('P');
+		alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+		alerta.innerHTML = `
 		<strong class="font-bold">¡Error!</strong>
 		<span class="block sm:inline">${mensaje}</span>
 	`;
-	// Insertando la alerta en HTML
-	formulario.appendChild(alerta);
-	// Eliminar la alerta después de 3 segundos
-	setTimeout(() => {
-		alerta.remove();
-	}, 3000);
+		// Insertando la alerta en HTML
+		formulario.appendChild(alerta);
+		// Eliminar la alerta después de 3 segundos
+		setTimeout(() => {
+			alerta.remove();
+		}, 3000);
+	}
 }
