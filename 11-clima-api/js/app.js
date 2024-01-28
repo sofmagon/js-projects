a34e1067227dc63ab48ffe5dc64c2c449bab3157
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-27 21:57:45 -0600

    feat: Spinner function

--- 11-clima-api/js/app.js
+++ 11-clima-api/js/app.js
@@ -116,3 +116,25 @@ function limpiarHTML() {
 		resultado.removeChild(resultado.firstChild);
 	}
 }
+
+function Spinner() {
+	// Creando el spinner con scripting
+	const divSpinner = document.createElement('DIV');
+	divSpinner.classList.add('sk-fading-circle');
+	divSpinner.innerHTML = `
+		<div class="sk-circle1 sk-circle"></div>
+		<div class="sk-circle2 sk-circle"></div>
+		<div class="sk-circle3 sk-circle"></div>
+		<div class="sk-circle4 sk-circle"></div>
+		<div class="sk-circle5 sk-circle"></div>
+		<div class="sk-circle6 sk-circle"></div>
+		<div class="sk-circle7 sk-circle"></div>
+		<div class="sk-circle8 sk-circle"></div>
+		<div class="sk-circle9 sk-circle"></div>
+		<div class="sk-circle10 sk-circle"></div>
+		<div class="sk-circle11 sk-circle"></div>
+		<div class="sk-circle12 sk-circle"></div>
+	`;
+
+	resultado.appendChild(divSpinner);
+}
