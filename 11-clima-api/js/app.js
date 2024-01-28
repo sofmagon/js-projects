7da46f5ecae0a1f70bf54e5f9baf3299f91fe937
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-27 20:24:52 -0600

    verifying if there's a previous alert

--- 11-clima-api/js/app.js
+++ 11-clima-api/js/app.js
@@ -23,4 +23,19 @@ function buscarClima(e) {
 
 function mostrarError(mensaje) {
 	console.log(mensaje);
+
+	// Verificar si ya existe una alerta
+	const alerta = document.querySelector('.bg-red-100');
+	if (!alerta) {
+		// Si no existe, crear alerta con scripting
+		const alerta = document.createElement('DIV');
+		alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+		alerta.innerHTML = `
+		<strong class="font-bold">¡Error!</strong>
+		<span class="block">${mensaje}</span>
+		`;
+
+		container.appendChild(alerta);
+	}
 }
