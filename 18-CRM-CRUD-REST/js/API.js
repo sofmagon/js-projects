9a7dd450cab8b827985a622f064c2173ed1d9d88
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-30 12:08:34 -0600

    add: fetch with its properties

--- 18-CRM-CRUD-REST/js/API.js
+++ 18-CRM-CRUD-REST/js/API.js
@@ -1,5 +1,19 @@
 const url = 'http://localhost:4000/clientes';
 
 export const nuevoCliente = cliente => {
-	console.log(cliente);
+	// console.log(cliente);
+
+	try {
+		fetch(url, {
+			method: 'POST',
+			// body se envía de 2 formas: string u objeto
+			body: JSON.stringify(cliente),
+			headers: {
+				// esto no sube archivos, sólo texto en JSON
+				'Content-Type': 'application/json'
+			}
+		})
+	} catch (error) {
+		console.log(error);
+	}
 }
