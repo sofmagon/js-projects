c82a6de7c083bb5f51beee253baf2d12f6cff62f
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-30 14:02:36 -0600

    feat: getting customer's ID by URLSearchParams on DOMContentLoaded event

---
+++ 18-CRM-CRUD-REST/js/editarCliente.js
@@ -0,0 +1,9 @@
+(function () {
+	document.addEventListener('DOMContentLoaded', () => {
+		// URLSearchParams, define métodos para trabajar con los parámetros de búsqueda de una URL
+		const parametrosURL = new URLSearchParams(window.location.search);
+		// Entonces se obtiene el ID del cliente deseado
+		const idCliente = parseInt(parametrosURL.get('id'));
+		console.log(idCliente);
+	})
+})();
