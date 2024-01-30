f050c27eec67a7d0d3bc96c1778896b68817ad81
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-30 12:41:47 -0600

    add: array method & object destructuring

--- 18-CRM-CRUD-REST/js/app.js
+++ 18-CRM-CRUD-REST/js/app.js
@@ -8,6 +8,13 @@ import { obtenerClientes } from "./API.js";
 	async function mostrarClientes() {
 		const clientes = await obtenerClientes();
 		// Async/await permite bloquear el siguiente código hasta que DOMContentLoaded esté listo o seguirá mostrando pending
-		console.log(clientes);
+		// console.log(clientes);
+
+
+		// Recorrer el array de objetos
+		clientes.forEach(cliente => {
+			// Destructuring
+			const { nombre, email, telefono, empresa, id } = cliente;
+		});
 	}
 })();
