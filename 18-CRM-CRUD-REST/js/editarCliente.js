064c8393bb234baff15679946de4d9520a0a2a6a
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-30 14:13:12 -0600

    add: import obtenerCliente from API.js

--- 18-CRM-CRUD-REST/js/editarCliente.js
+++ 18-CRM-CRUD-REST/js/editarCliente.js
@@ -1,9 +1,11 @@
+import { obtenerCliente } from "./API.js";
+
 (function () {
 	document.addEventListener('DOMContentLoaded', () => {
 		// URLSearchParams, define métodos para trabajar con los parámetros de búsqueda de una URL
 		const parametrosURL = new URLSearchParams(window.location.search);
 		// Entonces se obtiene el ID del cliente deseado
 		const idCliente = parseInt(parametrosURL.get('id'));
-		console.log(idCliente);
+		obtenerCliente(idCliente);
 	})
 })();
