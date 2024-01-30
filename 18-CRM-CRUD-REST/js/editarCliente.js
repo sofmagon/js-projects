198d2a377df9a69686b18dcf2ee37daab0eb1652
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-30 14:23:48 -0600

    add: async/await when calling obtenerCliente()

--- 18-CRM-CRUD-REST/js/editarCliente.js
+++ 18-CRM-CRUD-REST/js/editarCliente.js
@@ -1,11 +1,15 @@
 import { obtenerCliente } from "./API.js";
 
 (function () {
-	document.addEventListener('DOMContentLoaded', () => {
+	// Async/await permite bloquear el siguiente código hasta que la promesa de la API esté lista
+
+	document.addEventListener('DOMContentLoaded', async () => {
 		// URLSearchParams, define métodos para trabajar con los parámetros de búsqueda de una URL
 		const parametrosURL = new URLSearchParams(window.location.search);
 		// Entonces se obtiene el ID del cliente deseado
 		const idCliente = parseInt(parametrosURL.get('id'));
-		obtenerCliente(idCliente);
+		// Almacenando el objeto obtenido por la API
+		const cliente = await obtenerCliente(idCliente);
+		console.log(cliente);
 	})
 })();
