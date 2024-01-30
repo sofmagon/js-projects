daec9a4789cc862d89e0842ed86556cc80d3af00
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-30 12:33:30 -0600

    feat: mostrarClientes function

---
+++ 18-CRM-CRUD-REST/js/app.js
@@ -0,0 +1,9 @@
+(function () {
+	const listado = document.querySelector('#listado-clientes');
+
+	document.addEventListener('DOMContentLoaded', mostrarClientes);
+
+	function mostrarClientes() {
+
+	}
+})();
