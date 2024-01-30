e270d325f174dbe17cf42dfb35c7160bd20d526b
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-01-30 11:36:43 -0600

    feat: validarCliente function

--- 18-CRM-CRUD-REST/js/nuevoCliente.js
+++ 18-CRM-CRUD-REST/js/nuevoCliente.js
@@ -2,4 +2,24 @@
 (function () {
 	const formulario = document.querySelector('#formulario');
 	formulario.addEventListener('submit', validarCliente);
+
+	function validarCliente(e) {
+		e.preventDefault();
+
+		const nombre = document.querySelector('#nombre').value;
+		const email = document.querySelector('#email').value;
+		const telefono = document.querySelector('#telefono').value;
+		const empresa = document.querySelector('#empresa').value;
+
+		// Object literal enhancement
+		const cliente = {
+			nombre,
+			email,
+			telefono,
+			empresa
+		}
+
+		// Si al menos uno está vacío retorna true
+		console.log(!Object.values(cliente).every(input => input !== ''));
+	}
 })();
